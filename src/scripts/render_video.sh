bb5d823da3ae81e6a711075d6ad1d70cf9ccda87
Sachin Hrishikesh <sachinoncla@gmail.com>
2026-02-22 09:51:22 +0530

    Update render_video.sh

--- src/scripts/render_video.sh
+++ src/scripts/render_video.sh
@@ -6,11 +6,6 @@ cam_path="demo_data/campath.json"
 
 ckpt_dir="2026-02-19_064415"
 
-ns-render camera-path \
-    --camera-path-filename $cam_path \
-    --load-config ${exp_dir}/test/splatfactox/${ckpt_dir}/config.yml \
-    --output-path ${exp_dir}/video.mp4 \
-
 
 # 3. render each frame in the video.
 ns-render camera-path \
