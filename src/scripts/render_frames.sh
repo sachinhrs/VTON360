8da87ece982c71e88f0242838a4b27111dd49357
Sachin Hrishikesh <sachinoncla@gmail.com>
2026-02-22 10:30:55 +0530

    Create render_frames.sh

---
+++ src/scripts/render_frames.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -exu
+exp_dir="output"
+cam_path="demo_data/campath.json"
+
+ckpt_dir="2026-02-19_064415"
+
+
+# 3. render each frame in the video.
+ns-render camera-path \
+    --output-format images \
+    --camera-path-filename $cam_path \
+    --load-config ${exp_dir}/test/splatfactox/${ckpt_dir}/config.yml \
+    --output-path ${exp_dir}/frames/ \
