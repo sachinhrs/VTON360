c8e5caec0baca335edabeadd1cc770d6e546327a
Sachin Hrishikesh <sachinoncla@gmail.com>
2026-03-04 09:17:33 +0530

    Update infer_tryon_multi.py

--- src/multiview_consist_edit/infer_tryon_multi.py
+++ src/multiview_consist_edit/infer_tryon_multi.py
@@ -44,7 +44,7 @@ torch.cuda.manual_seed(seed)
 
 # dataset
 infer_data_config = config.infer_data
-i#f 'mvhumannet' in infer_data_config['dataroot']:
+#f 'mvhumannet' in infer_data_config['dataroot']:
 #    infer_dataset = MVHumanNet_Dataset(**infer_data_config)
 #    print('using mvhumannet')
 #else:
