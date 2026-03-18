6bc29a3af6d712c58b1456ef83e1e5ec67965d7f
Sachin Hrishikesh <sachinoncla@gmail.com>
2026-03-18 11:54:11 +0530

    Update geometry.py
    FORCE vertex colors for testing:

--- src/render_from_thuman/taichi_three/geometry.py
+++ src/render_from_thuman/taichi_three/geometry.py
@@ -72,12 +72,17 @@ def render_triangle(model, camera, face, lights):
                 continue
             
             coor = (ta * w_A + tb * w_B + tc * w_C)
-            if model.type[None] == 1:
-                camera.img[X] = (ca * w_A + cb * w_B + cc * w_C)
-            elif model.type[None] == 2:
-                camera.img[X] = color * (ca * w_A + cb * w_B + cc * w_C)
-            else:    
-                camera.img[X] = color * model.texSample(coor)
+                        # FORCE vertex colors for testing:
+            camera.img[X] = (ca * w_A + cb * w_B + cc * w_C)
+            camera.mask[X] = ti.Vector([1.0, 1.0, 1.0], ti.f32)
+            camera.normal_map[X] = -normal / 2.0 + 0.5
+            #if model.type[None] == 1:
+                #camera.img[X] = (ca * w_A + cb * w_B + cc * w_C)
+            #elif model.type[None] == 2:
+                #camera.img[X] = color * (ca * w_A + cb * w_B + cc * w_C)
+            #else:    
+                #camera.img[X] = color * model.texSample(coor)
+                
             camera.mask[X] = ti.Vector([1.0, 1.0, 1.0], ti.f32)
             camera.normal_map[X] = -normal / 2.0 + 0.5
             # camera.normal_map[X] = ts.vec3(shading / 2.0 + 0.5)
